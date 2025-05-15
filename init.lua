4049dfdda5f2879b39e8523ecb03657d5e1ba38e
artemisfowl <skb655952@gmail.com>
2025-05-16 00:04:26 +0530

    Removed lsplens support

--- init.lua
+++ init.lua
@@ -1255,32 +1255,6 @@ require('lazy').setup({
   --]]
 
   {
-    "VidocqH/lsp-lens.nvim",
-    name = "lsplens",
-    priority = 1000,
-    init = function()
-      local SymbolKind = vim.lsp.protocol.SymbolKind
-      require('lsp-lens').setup({
-        enable = true,
-        include_declaration = false,      -- Reference include declaration
-        sections = {                      -- Enable / Disable specific request, formatter example looks 'Format Requests'
-          definition = false,
-          references = true,
-          implements = true,
-          git_authors = true,
-        },
-        ignore_filetype = {
-          "prisma",
-        },
-        -- Target Symbol Kinds to show lens information
-        target_symbol_kinds = { SymbolKind.Function, SymbolKind.Method, SymbolKind.Interface },
-        -- Symbol Kinds that may have target symbol kinds as children
-        wrapper_symbol_kinds = { SymbolKind.Class, SymbolKind.Struct },
-      })
-    end
-  },
-
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
