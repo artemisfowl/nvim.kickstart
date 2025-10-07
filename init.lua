e3587cb078572a625fc309cac3e4f101bab21400
oldgod <oldgod@oldgods-MacBook-Air.local>
2025-10-08 01:52:59 +0530

    Added Addon loading for Love2D

--- init.lua
+++ init.lua
@@ -802,6 +802,10 @@ require('lazy').setup({
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+              workspace = {
+                userThirdParty = { os.getenv 'HOME' .. '.local/share/LuaAddons' },
+                checkThirdParty = 'Apply',
+              },
             },
           },
         },
