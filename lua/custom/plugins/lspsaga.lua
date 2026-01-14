1015e9ec1fe5864f6d9efe61c4f261d274f8ac46
artemisfowl <skb655952@gmail.com>
2026-01-15 04:25:24 +0530

    Added better breadcrumbs

--- lua/custom/plugins/lspsaga.lua
+++ lua/custom/plugins/lspsaga.lua
@@ -22,6 +22,9 @@ return {
         border = 'rounded',
         auto_preview = true,
       },
+      symbol_in_winbar = {
+        enable = false,
+      },
     }
   end,
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
